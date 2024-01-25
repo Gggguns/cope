899820c2396725b1217ff94f111ad2bf15736516
Gggguns <2523040452@qq.com>
2024-01-25 15:46:08 +0800

    小凯手中有两种面值的金币，两种面值均为正整数且彼此互素。每种金币小凯都有无数个。在不找零的情况下，仅凭这两种金币，有些物品他是无法准确支付的。现在小凯想知道在无法准确支付的物品中，最贵的价值是多少金币？注意：输入数据保证存在小凯无法准确支付的商品。输入数据仅一行，包含两个正整数a和b，它们之间用一个空格隔开，表示小凯手中金币的面值。其中，1≤a，b≤109 【输入格式】 输出仅一行，一个正整数N，表示不找零的情况下，小凯用手中的金币不能准确支付的最贵的物品的价值。 【输入样例】 3 7 【输出样例】 11

---
+++ text_2024_1_1_4_8.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int main()
+{
+	int Num1 = 0, Num2 = 0;
+	cin >> Num1 >> Num2;
+	cout << ((Num1 * Num2) - Num1 - Num2);
+	return 0;
+}
